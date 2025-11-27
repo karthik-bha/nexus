7015e8239ccee1fa953af80e2ed89ced73a88b9b
karthik-bha <143622670+karthik-bha@users.noreply.github.com>
2025-11-27 19:54:59 +0530

    fix: updated casing

--- nexus-frontend/src/main.jsx
+++ nexus-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import { SocketProvider } from './context/socketContext.jsx';
+import { SocketProvider } from './context/SocketContext.jsx';
 import { ChatProvider } from './context/ChatContext.jsx';
 
 createRoot(document.getElementById('root')).render(
