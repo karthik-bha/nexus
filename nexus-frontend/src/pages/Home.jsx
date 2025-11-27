59b1e455c53d8aad99ef4b8353f1f1bc59b17835
karthik-bha <143622670+karthik-bha@users.noreply.github.com>
2025-11-27 20:16:04 +0530

    ft: updated vercel directives

--- nexus-frontend/src/pages/Home.jsx
+++ nexus-frontend/src/pages/Home.jsx
@@ -1,23 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
+    const navigate = useNavigate(); 
+
     return (
         <div className="h-[100vh] flex items-center justify-center bg-black text-gray-100">
             <div className="text-center z-50 px-4">
-                {/* Sleek Nexus style */}
-                {/* <h1 className="text-5xl md:text-6xl font-semibold tracking-widest mb-4 text-white">
-                    NEXUS
-                </h1> */}
                 <img src="/nexus-logo.jpg" alt="Nexus Logo" className="w-full md:w-[550px] h-auto mx-auto mb-6" />
                 <h2 className="text-gray-300 mb-6">The Nexus for all the happening</h2>
                 <div className="flex flex-col md:flex-row justify-center gap-4 mt-4">
                     <button
                         className="hover:cursor-pointer bg-gray-100 text-black font-semibold py-2 px-6 rounded-full hover:bg-gray-300 transition-all duration-200"
-                        onClick={() => window.location.href = "/auth?type=sign-up"}
+                        onClick={() => navigate("/auth?type=sign-up")} 
                     >
                         Sign Up
                     </button>
                     <button
                         className="hover:cursor-pointer bg-transparent border border-gray-400 text-gray-200 font-semibold py-2 px-6 rounded-full hover:bg-gray-800 hover:border-gray-200 transition-all duration-200"
-                        onClick={() => window.location.href = "/auth?type=sign-in"}
+                        onClick={() => navigate("/auth?type=sign-in")}
                     >
                         Sign In
                     </button>
